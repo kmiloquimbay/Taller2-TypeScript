8d9d56f915d18256dc7f48bc21e8a0f2b1c6dc74
kmiloquimbay <d.quimbay@uniandes.edu.co>
2024-10-20 23:04:11 -0500

    commit final

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,10 @@ function showSeries(series: any[]): void {
             <td>${serie.getChannel()}</td>
             <td>${serie.getSeasons()}</td>
         `;
+
+        serieRow.addEventListener('click', () => {
+            showInfoSerie(serie);
+        });
         totalSeasons += serie.getSeasons();
 
         seriesList.appendChild(serieRow);
@@ -28,8 +32,25 @@ function showSeries(series: any[]): void {
     const averageSeasons = Math.round(totalSeasons / series.length);
 
     let averageText: HTMLElement = document.createElement('p');
-    averageText.className = 'mt-3';
+    averageText.className = 'mt-3 avg-text';
     averageText.textContent = `Average number of seasons: ${averageSeasons}`;
 
     seriesTable.parentElement!.appendChild(averageText);
+}
+
+function showInfoSerie(serie: any): void {
+    let cardContainer: HTMLElement = document.getElementById('cardContainer')!;
+    let serieImage: HTMLImageElement = document.getElementById('serieImage') as HTMLImageElement;
+    let serieTitle: HTMLElement = document.getElementById('serieTitle')!;
+    let serieDescription: HTMLElement = document.getElementById('serieDescription')!;
+    let serieLink: HTMLAnchorElement = document.getElementById('serieLink') as HTMLAnchorElement;
+
+    // Actualizar los valores de la tarjeta con los detalles de la serie
+    serieImage.src = serie.getImage();
+    serieTitle.textContent = serie.getName();
+    serieDescription.textContent = serie.getDescription();
+    serieLink.href = serie.getLink();
+
+    // Mostrar la tarjeta si está oculta
+    cardContainer.style.display = 'block';
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,37 @@ showSeries(series);
 function showSeries(series) {
     var seriesList = document.createElement('tbody');
     var totalSeasons = 0;
-    for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
-        var serie = series_1[_i];
+    var _loop_1 = function (serie) {
         var serieRow = document.createElement('tr');
         serieRow.innerHTML = "\n            <td>".concat(serie.getId(), "</td>\n            <td class=\"series-name\">").concat(serie.getName(), "</td>\n            <td>").concat(serie.getChannel(), "</td>\n            <td>").concat(serie.getSeasons(), "</td>\n        ");
+        serieRow.addEventListener('click', function () {
+            showInfoSerie(serie);
+        });
         totalSeasons += serie.getSeasons();
         seriesList.appendChild(serieRow);
+    };
+    for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
+        var serie = series_1[_i];
+        _loop_1(serie);
     }
     seriesTable.appendChild(seriesList);
     var averageSeasons = Math.round(totalSeasons / series.length);
     var averageText = document.createElement('p');
-    averageText.className = 'mt-3';
+    averageText.className = 'mt-3 avg-text';
     averageText.textContent = "Average number of seasons: ".concat(averageSeasons);
     seriesTable.parentElement.appendChild(averageText);
+}
+function showInfoSerie(serie) {
+    var cardContainer = document.getElementById('cardContainer');
+    var serieImage = document.getElementById('serieImage');
+    var serieTitle = document.getElementById('serieTitle');
+    var serieDescription = document.getElementById('serieDescription');
+    var serieLink = document.getElementById('serieLink');
+    // Actualizar los valores de la tarjeta con los detalles de la serie
+    serieImage.src = serie.getImage();
+    serieTitle.textContent = serie.getName();
+    serieDescription.textContent = serie.getDescription();
+    serieLink.href = serie.getLink();
+    // Mostrar la tarjeta si está oculta
+    cardContainer.style.display = 'block';
 }
