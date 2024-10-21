8ba11e0946b9907b045f72060cf80193940dfe2e
kmiloquimbay <d.quimbay@uniandes.edu.co>
2024-10-20 20:00:29 -0500

    commit inicial

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { series } from './data.js';
+
+for (let serie of series) {
+    console.log(serie);
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,5 @@
+import { series } from './data.js';
+for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
+    var serie = series_1[_i];
+    console.log(serie);
+}
