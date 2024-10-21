3288d2101e5ee949b06b35401de901cd5b95de64
kmiloquimbay <d.quimbay@uniandes.edu.co>
2024-10-20 20:53:10 -0500

    finish taller 1

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,35 @@
 import { series } from './data.js';
 
-for (let serie of series) {
-    console.log(serie);
+let seriesTable: HTMLElement = document.getElementById('seriesTable')!;
+
+showSeries(series);
+
+function showSeries(series: any[]): void {
+    let seriesList: HTMLElement = document.createElement('tbody');
+    
+
+    let totalSeasons = 0;
+
+    for(let serie of series) {
+        let serieRow: HTMLElement = document.createElement('tr');
+        serieRow.innerHTML = `
+            <td>${serie.getId()}</td>
+            <td>${serie.getName()}</td>
+            <td>${serie.getChannel()}</td>
+            <td>${serie.getSeasons()}</td>
+        `;
+        totalSeasons += serie.getSeasons();
+
+        seriesList.appendChild(serieRow);
+    }
+
+    seriesTable.appendChild(seriesList); 
+
+    const averageSeasons = Math.round(totalSeasons / series.length);
+
+    let averageText: HTMLElement = document.createElement('p');
+    averageText.className = 'mt-3';
+    averageText.textContent = `Average number of seasons: ${averageSeasons}`;
+
+    seriesTable.parentElement!.appendChild(averageText);
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 import { series } from './data.js';
-for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
-    var serie = series_1[_i];
-    console.log(serie);
+var seriesTable = document.getElementById('seriesTable');
+showSeries(series);
+function showSeries(series) {
+    var seriesList = document.createElement('tbody');
+    var totalSeasons = 0;
+    for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
+        var serie = series_1[_i];
+        var serieRow = document.createElement('tr');
+        serieRow.innerHTML = "\n            <td>".concat(serie.getId(), "</td>\n            <td>").concat(serie.getName(), "</td>\n            <td>").concat(serie.getChannel(), "</td>\n            <td>").concat(serie.getSeasons(), "</td>\n        ");
+        totalSeasons += serie.getSeasons();
+        seriesList.appendChild(serieRow);
+    }
+    seriesTable.appendChild(seriesList);
+    var averageSeasons = Math.round(totalSeasons / series.length);
+    var averageText = document.createElement('p');
+    averageText.className = 'mt-3';
+    averageText.textContent = "Average number of seasons: ".concat(averageSeasons);
+    seriesTable.parentElement.appendChild(averageText);
 }
